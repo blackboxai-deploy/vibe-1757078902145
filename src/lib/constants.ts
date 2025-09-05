e2abb3c76a3f7156539912f26bc0466b5b287bd6
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-05 18:58:59 +0530

    Add src/lib/constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,87 @@
+export const COMPANY_INFO = {
+  name: 'Josivan Sousa Corretor de Imóveis',
+  owner: 'Josivan Sousa',
+  creci: 'CRECI RJ-051628',
+  phone: '(21) 99999-9999',
+  whatsapp: '5521999999999',
+  email: 'contato@josivansousaimoveis.com.br',
+  address: {
+    street: 'Rua das Flores, 123',
+    neighborhood: 'Copacabana',
+    city: 'Rio de Janeiro',
+    state: 'RJ',
+    zipCode: '22070-030'
+  },
+  socialMedia: {
+    instagram: '@josivansousaimoveis',
+    facebook: 'josivansousaimoveis',
+    linkedin: 'josivan-sousa'
+  },
+  workingHours: {
+    weekdays: '08:00 - 18:00',
+    saturday: '08:00 - 14:00',
+    sunday: 'Fechado'
+  }
+};
+
+export const PROPERTY_TYPES = [
+  { value: 'apartment', label: 'Apartamento' },
+  { value: 'house', label: 'Casa' },
+  { value: 'commercial', label: 'Comercial' },
+  { value: 'land', label: 'Terreno' }
+];
+
+export const PROPERTY_STATUS = [
+  { value: 'sale', label: 'Venda' },
+  { value: 'rent', label: 'Aluguel' }
+];
+
+export const NEIGHBORHOODS = [
+  'Copacabana',
+  'Ipanema',
+  'Leblon',
+  'Barra da Tijuca',
+  'Botafogo',
+  'Flamengo',
+  'Tijuca',
+  'Vila Isabel',
+  'Méier',
+  'Centro',
+  'Santa Teresa',
+  'Lapa'
+];
+
+export const BEDROOM_OPTIONS = [
+  { value: 1, label: '1 quarto' },
+  { value: 2, label: '2 quartos' },
+  { value: 3, label: '3 quartos' },
+  { value: 4, label: '4 quartos' },
+  { value: 5, label: '5+ quartos' }
+];
+
+export const COMMON_FEATURES = [
+  'Piscina',
+  'Academia',
+  'Churrasqueira',
+  'Playground',
+  'Salão de festas',
+  'Portaria 24h',
+  'Elevador',
+  'Ar condicionado',
+  'Varanda',
+  'Suíte',
+  'Closet',
+  'Garagem',
+  'Jardim',
+  'Terraço',
+  'Vista para o mar',
+  'Mobiliado',
+  'Quintal'
+];
+
+export const SEO_CONFIG = {
+  title: 'Josivan Sousa Corretor de Imóveis - CRECI RJ-051628',
+  description: 'Encontre o imóvel dos seus sonhos no Rio de Janeiro. Josivan Sousa, corretor especializado com anos de experiência no mercado imobiliário.',
+  keywords: 'imóveis, corretor, Rio de Janeiro, apartamento, casa, venda, aluguel, CRECI',
+  siteUrl: 'https://josivansousaimoveis.com.br'
+};
